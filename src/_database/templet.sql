26ab355906bc70ab67680e8809a6b701d4707faf
Sharif15 <Sharifpial225@gmail.com>
2025-10-11 19:52:02 -0600

    fixed comment and droping table if exists

--- src/_database/templet.sql
+++ src/_database/templet.sql
@@ -1,11 +1,9 @@
-```
+/*
 Purpose : Using sql file to systematically generete the Postgresql data base for my application 
 
 @author : Sharif Islam
 
-```
 
-```
 How to use : 
 
 From the terminal run the following command : 
@@ -14,13 +12,14 @@ psql -U "user_name" -d "database_name" -f "sql_file_name"
 
 replace the user_name, database_name and sql_file_name with the appropriate names  
 
-```
+*/
 
 
 -- ===========================
 -- USERS TABLE
 -- ===========================
 
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -32,6 +31,7 @@ CREATE TABLE users (
 -- INCOME TABLE
 -- ===========================
 
+DROP TABLE IF EXISTS income CASCADE;
 CREATE TABLE income (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
@@ -45,6 +45,7 @@ CREATE TABLE income (
 -- CATEGORY TABLE
 -- ===========================
 
+DROP TABLE IF EXISTS category CASCADE;
 CREATE TABLE category (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL
@@ -54,6 +55,7 @@ CREATE TABLE category (
 -- SPENDING TABLE
 -- ===========================
 
+DROP TABLE IF EXISTS spending CASCADE;
 CREATE TABLE spending (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
